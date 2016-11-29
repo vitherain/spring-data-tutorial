46d7251cefacce0a3e77173e9d7cd0d90a8f6f1e
Vit Herain <vit.herain@inventi.cz>
2016-11-29 12:48:33 +0100

    First commit

---
+++ src/main/java/cz/herain/tutorial/SpringDataTutorialApplication.java
@@ -0,0 +1,12 @@
+package cz.herain.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataTutorialApplication.class, args);
+	}
+}
